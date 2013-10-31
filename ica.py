dd8b2dee299756601fb3645b529cee416faa71d3
Sammie VB <sammie.katt@gmail.com>
2013-10-31 16:06:16 +0100

    First file

---
+++ ica.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sawtooth(x, period=0.2, amp=1.0, phase=0.):
+  return (((x / period - phase - 0.5) % 1) - 0.5) * 2 * amp
+
+def sine_wave(x, period=0.2, amp=1.0, phase=0.):
+  return np.sin((x / period - phase) * 2 * np.pi) * amp
+
+def square_wave(x, period=0.2, amp=1.0, phase=0.):
+  return ((np.floor(2 * x / period - 2 * phase - 1) % 2 == 0).astype(float) - 0.5) * 2 * amp
+
+def triangle_wave(x, period=0.2, amp=1.0, phase=0.):
+  return (sawtooth(x, period, 1., phase) * square_wave(x, period, 1., phase) + 0.5) * 2 * amp
+
+def random_nonsingular_matrix(d=2):
+  """
+  Generates a random nonsingular (invertible) matrix if shape d*d
+  """
+  epsilon = 0.1
+  A = np.random.rand(d, d)
+  while abs(np.linalg.det(A)) < epsilon:
+  A = np.random.rand(d, d)
+  return A
+
+def plot_signals(X):
+  """
+  Plot the signals contained in the rows of X.  
+  """
+  figure()
+  for i in range(X.shape[0]):
+    ax = plt.subplot(X.shape[0], 1, i + 1)
+    plot(X[i, :])
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+
