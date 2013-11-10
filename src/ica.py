bea973d37a070124e4d9b05bc2015f7ce0e77138
RR <hypersqual@gmail.com>
2013-11-10 23:26:19 +0100

    uncoment scipy

--- src/ica.py
+++ src/ica.py
@@ -194,7 +194,7 @@ def test_ICA():
 
     # create data
     data = generate_data()
-    plot_scatter(sources,"Sources")
+    plot_scatter(data,"Sources")
     mixed_data = np.dot(random_nonsingular_matrix(data.shape[0]), data)
 
     # perform ica
