9474b188ba8cd46d867d1484d2be1ab633a8f836
Michael Cabot <michael.a.cabot@gmail.com>
2013-11-25 14:23:00 +0100

    test factor to node

--- src/spa.py
+++ src/spa.py
@@ -7,19 +7,19 @@ class Node(object):
     def __init__(self, name):
         # A name for this Node, for printing purposes
         self.name = name
-         
+
         # Neighbours in the graph, identified with their index in this list.
         # i.e. self.neighbours contains neighbour 0 through len(self.neighbours) - 1.
         self.neighbours = []
-        
+
         # Reset the node-state (not the graph topology)
         self.reset()
-        
+
     def reset(self):
         # Incomming messages; a dictionary mapping neighbours to messages.
         # That is, it maps  Node -> np.ndarray.
         self.in_msgs = {}
-        
+
         # A set of neighbours for which this node has pending messages.
         # We use a python set object so we don't have to worry about duplicates.
         self.pending = set([])
@@ -30,18 +30,18 @@ class Node(object):
     def send_sp_msg(self, other):
         # To be implemented in subclass.
         raise Exception('Method send_sp_msg not implemented in base-class Node')
-   
+
     def send_ms_msg(self, other):
         # To be implemented in subclass.
         raise Exception('Method send_ms_msg not implemented in base-class Node')
-    
+
     def receive_msg(self, other, msg):
         # Store the incomming message, replacing previous messages from the same node
         self.in_msgs[other] = msg
 
         # TODO: add pending messages
         # self.pending.update(...)
-    
+
     def __str__(self):
         # This is printed when using 'print node_instance'
         return self.name
@@ -52,17 +52,17 @@ class Variable(Node):
         """
         Variable node constructor.
         Args:
-            name: a name string for this node. Used for printing. 
+            name: a name string for this node. Used for printing.
             num_states: the number of states this variable can take.
             Allowable states run from 0 through (num_states - 1).
             For example, for a binary variable num_states=2,
             and the allowable states are 0, 1.
         """
         self.num_states = num_states
-        
+
         # Call the base-class constructor
         super(Variable, self).__init__(name)
-    
+
     def set_observed(self, observed_state):
         """
         Set this variable to an observed state.
@@ -73,7 +73,7 @@ class Variable(Node):
         # Could be 0.0 for sum-product, but log(0.0) = -inf so a tiny value is preferable for max-sum
         self.observed_state[:] = 0.000001
         self.observed_state[observed_state] = 1.0
-        
+
     def set_latent(self):
         """
         Erase an observed state for this variable and consider it latent again.
@@ -82,11 +82,11 @@ class Variable(Node):
         # Using this representation we need not differentiate observed an latent
         # variables when sending messages.
         self.observed_state[:] = 1.0
-        
+
     def reset(self):
         super(Variable, self).reset()
         self.observed_state = np.ones(self.num_states)
-        
+
     def marginal(self, Z=None):
         """
         Compute the marginal distribution of this Variable.
@@ -97,11 +97,11 @@ class Variable(Node):
         """
         # TODO: compute marginal
         return None, None
-    
+
     def send_sp_msg(self, other):
         # TODO: implement Variable -> Factor message for sum-product
         pass
-   
+
     def send_ms_msg(self, other):
         # TODO: implement Variable -> Factor message for max-sum
         pass
@@ -121,7 +121,7 @@ class Factor(Node):
         super(Factor, self).__init__(name)
 
         assert len(neighbours) == f.ndim, 'Factor function f should accept as many arguments as this Factor node has neighbours'
-        
+
         for nb_ind in range(len(neighbours)):
             nb = neighbours[nb_ind]
             assert f.shape[nb_ind] == nb.num_states, 'The range of the factor function f is invalid for input %i %s' % (nb_ind, nb.name)
@@ -129,26 +129,23 @@ class Factor(Node):
             nb.add_neighbour(self)
 
         self.f = f
-        
+
     def send_sp_msg(self, other):
         # TODO: implement Factor -> Variable message for sum-product
 
         # check if all required information is available
 
         # compute msg
-        messages = [np.array(message) for message in self.in_msgs.itervalues()]
-        
-        # todo: make them same order as factor (thus same order as neighbours)
-        messages = np.multiply.reduce(np.ix_(*messages))
+        messages = [self.in_msgs[node] for node in self.neighbours if node != other]
+        messages_prod = np.multiply.reduce(np.ix_(*messages))
 
-        # fix this
-        factorDims = range(self.f.ndim)
-        msg = np.tensordot(messages, self.f, range(messages.ndim), range(self.f.ndim))
-        # end fix
+        factor_dims = range(self.f.ndim)
+        factor_dims.pop(self.neighbours.index(other))
+        msg = np.tensordot(messages_prod, self.f, range(messages_prod.ndim), factor_dims)
 
         # send msg
         other.receive_msg(self, msg)
-   
+
     def send_ms_msg(self, other):
         # TODO: implement Factor -> Variable message for max-sum
         pass
@@ -161,7 +158,7 @@ def instantiate1():
     # holds the nodes in graph
     nodes = dict()
 
-    # append the nodes 
+    # append the nodes
     varNames = ['Influenza', 'SoreThroat', 'Fever', 'Bronchitis', \
         'Smokes', 'Wheezing', 'Coughing']
     for varName in varNames:
@@ -182,6 +179,17 @@ def instantiate1():
 
     return nodes
 
+def test_factor_to_variable():
+    graph = instantiate1()
+    factor = graph['BR-IN-SM']
+    messages = [np.array([1, 2]), np.array([3, 4]), np.array([5, 6])]
+    for i, message in enumerate(messages):
+        factor.in_msgs[factor.neighbours[i]] = message
+
+    for node in factor.neighbours:
+        factor.send_sp_msg(node)
+        print node.in_msgs[factor]
+
 ###### Debugging functions ######
 def print_graph(graph):
     """
@@ -193,5 +201,6 @@ def print_graph(graph):
             (str(node).ljust(20), len(node.neighbours), str(type(node))))
 
 if __name__ == '__main__':
-    graph = instantiate1()
-    print_graph(graph)
+    #graph = instantiate1()
+    #print_graph(graph)
+    test_factor_to_variable()
