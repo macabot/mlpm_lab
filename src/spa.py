3702ff62e7c63715450181abad937c82905a57bf
samkatt <sammie.katt@gmail.com>
2013-11-28 00:12:32 +0100

    Merge and if statements in 1 line

--- src/spa.py
+++ src/spa.py
@@ -320,10 +320,8 @@ def graph_to_img(xnodes, dims):
     for i in range(dims[0]):
         for j in range(dims[1]):
             #print xnodes[dims[0]*i+j].max_state()
-            if xnodes[dims[1]*i+j].max_state() >= 1:
-                new_img[i][j] = 1
-            else: 
-                new_img[i][j] = 0
+            new_img[i][j] = int(xnodes[dims[1]*i+j].max_state() >= 1)
+
     plt.gray()            
     plt.imshow(new_img)
     plt.show()
@@ -344,19 +342,19 @@ def im_to_graph(im, fact_prob=[0.9,0.9,0.9]):
     neighbour_factor_up = np.array([[fact_prob[2], fact_prob[2]-1], [fact_prob[2]-1, fact_prob[2]]])
 
     # for each row in image
-    for i in range(im.shape[1]):
+    for i in range(im.shape[0]):
 
         # holds nodes in this row to be appended to x_nodes
         x_variable_row = []
 
         # for each individual pixel
-        for j in range(im.shape[0]):
+        for j in range(im.shape[1]):
             # create nodes for each pixel (latent and observed)
             variableY = Variable("Y_%d_%d" %(i, j), 2)
             variableX = Variable("X_%d_%d" %(i, j), 2)
             # set node to observed
             
-            variableY.set_observed(int(im[j,i]))
+            variableY.set_observed(int(im[i,j]))
             
             # set all factors
             X_Yfactor           = Factor('%s, %s'% (variableY.name,variableX.name), xy_factor , [variableY, variableX]) 
@@ -587,9 +585,9 @@ if __name__ == '__main__':
     #im_to_graph(im, [0.9, 0.95, 0.97])
 
     #test_graph_to_img('./dalmation2.png')
-    test_graph_to_img('../../lab2/dalmation2.png')
+    #test_graph_to_img('../../lab2/dalmation2.png')
     #test_loopy('../../lab2/dalmation2.png')
-    #denoise_img('../../lab2/dalmation2.png', [0.9, 0.95, 0.97])
+    denoise_img('../../lab2/dalmation2.png', [0.9, 0.95, 0.97])
 
     #test_img_type()
     #pl.show()
