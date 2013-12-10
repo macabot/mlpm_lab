c6d8fec672263d50e8cdb5e3912db6ce9cf630ec
Michael Cabot <michael.a.cabot@gmail.com>
2013-12-10 19:11:06 +0100

    update mu w and tau

--- src/vpca.py
+++ src/vpca.py
@@ -3,13 +3,13 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 class BayesianPCA(object):
-    
+
     def __init__(self, d, N, a_alpha=10e-3, b_alpha=10e-3, a_tau=10e-3, b_tau=10e-3, beta=10e-3):
         """
         """
         self.d = d # number of dimensions
         self.N = N # number of data points
-        
+
         # Hyperparameters
         self.a_alpha = a_alpha
         self.b_alpha = b_alpha
@@ -28,12 +28,12 @@ class BayesianPCA(object):
         self.bs_alpha_tilde = np.abs(np.random.randn(d, 1))
         self.a_tau_tilde = np.abs(np.random.randn(1))
         self.b_tau_tilde = np.abs(np.random.randn(1))
-    
+
         # set data TODO
         self.data = 0
 
     def __update_z(self, X):
-        """ 
+        """
         updates X (projection of data points?)
         X is the prod over datapoints: N(x_n | m_x, Sigma_n)
         X[0][n] stands for nth mean
@@ -42,7 +42,7 @@ class BayesianPCA(object):
         m_x = <tau> * E_x * <W^t> * (t_n - <mu>)
         E_x = (I + <tau> <W^t*W>)^-1
         """
-        
+
         # variables necessary in calculations
         tau_exp = self.a_tau_tilde / self.b_tau_tilde
         W_exp = np.multiply.reduce(self.means_w)
@@ -53,23 +53,43 @@ class BayesianPCA(object):
 
         # update mean
         X[0] = np.dot(np.dot( tau_exp * X[1], W_exp.T), (self.data - mu_exp))
-    
 
-    def __update_mu(self):
-        pass
+
+    def __update_mu(self, X):
+        """update mean_mu and sigma_mu"""
+        m_x, sigma_x = X
+        tau_exp = self.a_tau_tilde / self.b_tau_tilde
+
+        sum_t_w_x = np.sum(self.data - np.dot(self.means_w, m_x), axis=1)
+        self.mean_mu = np.dot(np.dot(tau_exp, self.sigma_mu), sum_t_w_x)
+
+        beta_n_tau-inv = 1.0 / (self.beta + self.N * tau_exp)
+        self.sigma_mu = beta_n_tau_inv * np.eye(self.d) # TODO check size of I
+
 
     def __update_w(self, X):
-        pass
-    
+        """update mean_w and sigma_w"""
+        m_x, sigma_x = X
+        tau_exp = self.a_tau_tilde / self.b_tau_tilde
+        sum_x_t_mu = np.sum(np.dot(m_x, (self.data - self.mean_mu)), axis=1)
+        self.mean_w = np.dot(np.dot(tau_exp, self.sigma_w), sum_x_t_mu)
+
+        diag_exp_alpha = np.diag(self.a_alpha_tilde / self.bs_alpha_tilde)
+        tau_sum_xn = tau_exp * np.sum(np.dot(m_x, m_x), axis=1)
+        self.sigma_w = np.linalg.inv(diag_exp_alpha + tau_sum_xn)
+
     def __update_alpha(self):
         pass
 
     def __update_tau(self, X):
-        pass
+        t_norm_sq = np.power(np.linalg.norm(self.data))
+        mu_norm_sq = np.power(np.linalg.norm(self.mean_mu))
+        t_mu = t_norm_sq + mu_norm_sq
+        # TODO continue
 
     def L(self, X):
         L = 0.0
         return L
-    
+
     def fit(self, X):
         pass
