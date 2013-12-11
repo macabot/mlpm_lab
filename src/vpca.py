9a11791622ab962b4745a7da41c9aa4d23935ddc
Michael Cabot <michael.a.cabot@gmail.com>
2013-12-11 22:12:18 +0100

    changes to update w

--- src/vpca.py
+++ src/vpca.py
@@ -33,14 +33,14 @@ class BayesianPCA(object):
         self.a_tau_tilde = np.abs(np.random.randn(1))
         self.b_tau_tilde = np.abs(np.random.randn(1))
 
-        # set data 
+        # set data
         Sigma = np.diag([5,4,3,2,1,1,1,1,1,1])
         self.data = np.random.multivariate_normal(np.zeros(d), Sigma, N).T
 
 
     def __update_z(self, X):
         """
-        updates X 
+        updates X
         X is the prod over datapoints: N(x_n | m_x, Sigma_n)
         X[0][n] stands for nth mean
         X[1] stands for sigma
@@ -71,12 +71,12 @@ class BayesianPCA(object):
         update mean_mu and sigma_mu
 
         sigma_mu = (beta + N * <tau>)^-1 * I
-        mean_mu = <tau> * sigma_mu * sum( data_n - <W> * <X_n> ) 
+        mean_mu = <tau> * sigma_mu * sum( data_n - <W> * <X_n> )
         """
 
         # necessary for calculations of both
         tau_exp = self.a_tau_tilde / self.b_tau_tilde
-       
+
         # update sigma_mu first as it is used in updating mean_mu
         self.sigma_mu = (1.0 / (self.beta + self.N * tau_exp)) * np.eye(self.d)
 
@@ -93,16 +93,14 @@ class BayesianPCA(object):
         sigma_w ( diag<alpha> + <tau> * sum( <x_n * x_n.T  )  )
         mean_w_k = <tau> * sigma_w * sum( <x_n> * ( t_nk - <mu_k>  )  )
         """
-
         # necessary for calculations
         tau_exp = self.a_tau_tilde / self.b_tau_tilde
 
-
         # update sigma_w first as it is used in updating means_w
-        m_x, sigma_x = X
-        # TODO: is the expected value over alpha not a mixture?
-        diag_exp_alpha = np.diag(self.a_alpha_tilde / self.b_alpha_tilde)
-        tau_sum_xn = tau_exp * np.sum(np.dot(m_x, m_x.T), axis=1) 
+        mean_x, sigma_x = X
+        a_over_b = self.a_alpha_tilde / self.b_alpha_tilde
+        diag_exp_alpha = np.diag(a_over_b.T[0])
+        tau_sum_xn = tau_exp * (self.N * sigma_x + np.dot(mean_x, mean_x.T))
         self.sigma_w = np.linalg.inv(diag_exp_alpha + tau_sum_xn)
 
         # update means_w
@@ -111,24 +109,24 @@ class BayesianPCA(object):
         self.means_w = np.dot(tau_exp * self.sigma_w, einsum_result)
 
     def __update_alpha(self):
-        """ 
-        update b_alpha_tilde, a_alpha_tilde does not change 
+        """
+        update b_alpha_tilde, a_alpha_tilde does not change
 
         b_alpha_tilde[i] = b_alpha + < || w[i] ||^2 > / 2
         """
 
-        # update each element in b_alpha_tilde 
+        # update each element in b_alpha_tilde
         # TODO: not sure, correction: pretty sure it is wrong
         w_norm = np.power(np.linalg.norm(self.means_w, axis=1),2)
-       
+
         self.b_alpha_tilde = self.b_alpha + w_norm / 2
 
     def __update_tau(self, X):
         """
         Update b_tau_tilde, as a_tau_tilde is independent of other update rules
 
-        b_tau_tilde = b_tau + 1/2 sum ( Z  ) 
-        where Z = 
+        b_tau_tilde = b_tau + 1/2 sum ( Z  )
+        where Z =
         || t_n || ^2 + <|| mu ||^2> + Tr(<W.T * W> <x_n * x_n.T>) +
             2 * <mu.T> * <W> * <x_n> - 2 * t_n.T * <W> * <x_n> - 2 * t_n.T * <mu>
         """
@@ -139,17 +137,17 @@ class BayesianPCA(object):
         exp_wt_w = np.trace(self.sigma_w) + np.dot(self.mean_w.T, self.mean_w)
         # x ~ N(m, Sigma): E[x x^T] = Sigma + m m^T
         exp_x_xt = sigma_x + np.dot(mean_x, mean_x.dot)
-        
+
         trace_w_x = np.trace(np.dot(exp_wt_w, exp_x_xt))
-        
+
         mu_w_x = np.dot(np.dot(self.mean_mu, self.mean_w), mean_x)
-        
+
         t_w_x = np.dot(np.dot(self.data.T, self.mean_w), mean_x)
-        
+
         t_mu = np.dot(self.data.T, self.mean_mu)
-        
+
         big_sum = np.sum(t_norm_sq + mu_norm_sq + trace_w_x + 2*mu_w_x - 2*t_w_x - 2*t_mu , axis=1)
-        
+
         self.b_tau_tilde = self.b_tau + 0.5*big_sum
 
     def L(self, X):
@@ -164,7 +162,7 @@ class BayesianPCA(object):
 
         it = 1000
         converged = False
-        while not converged and it > 0: 
+        while not converged and it > 0:
             print("Updating iteration " + str(it))
             # run each update
             self.__update_z(X)
@@ -179,10 +177,10 @@ class BayesianPCA(object):
 
     def test_shapes(self, X):
         """
-        This test simply tests whether the shapes of the 
+        This test simply tests whether the shapes of the
         important variables change. If they do, a warning is produced
         """
-        
+
 
         shapes = self.get_shapes()
         # run tests while updating
@@ -231,7 +229,7 @@ class BayesianPCA(object):
         assert orig_shapes[1] == self.sigma_mu.shape, 'sigma_mu: ' + str(self.sigma_mu.shape) + ', should have been: ' + str(orig_shapes[1])
 
         assert orig_shapes[2] == self.means_w.shape, 'means_w: ' + str(self.means_w.shape) + ', should have been: ' + str(orig_shapes[2])
-        
+
         assert orig_shapes[3] == self.sigma_w.shape, 'sigma_w: ' + str(self.sigma_w.shape) + ', should have been: ' + str(orig_shapes[3])
 
         assert orig_shapes[4] == self.b_alpha_tilde.shape, 'b_alpha_tilde: ' + str(self.b_alpha_tilde.shape) + ', should have been: ' + str(orig_shapes[4])
@@ -240,7 +238,7 @@ class BayesianPCA(object):
 
 
         print("Testing succceeded")
-   
+
 
 def run():
     vpca = BayesianPCA(10, 2)
